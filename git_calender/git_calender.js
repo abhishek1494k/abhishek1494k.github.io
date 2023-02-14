507e49eb602f982357395a25ef13346271f1f2c0
Abhishek <abhi.jaiswal1494@gmail.com>
2023-02-14 15:21:23 +0530

    Git

--- git_calender/git_calender.js
+++ git_calender/git_calender.js
@@ -4,8 +4,23 @@ GitHubCalendar(".calendar", "abhishek1494k");
 GitHubCalendar(".calendar", "abhishek1494k", { responsive: true });
 
 // Use a proxy
-// GitHubCalendar(".calendar", "abhishek1494k", {
-//   proxy(abhishek1494k) {
-//     return fetch(`https://your-proxy.com/github?user=${abhishek1494k}`);
-//   },
-// }).then((r) => r.text());
+GitHubCalendar(".calendar", "abhishek1494k", {
+  proxy(abhishek1494k) {
+    return fetch(`https://your-proxy.com/github?user=${abhishek1494k}`);
+  },
+}).then((r) => r.text());
+
+
+// import HeatCalendar from 'react-heat-calendar';
+
+// <HeatCalendar
+//   beginDate={new Date('2022-12-01')} // optional
+//   endDate={new Date('2023-01-31')}   // optional
+//   dateField="date"                   // optional
+//   data={[
+//     { date: '2016-12-02', someAttr: "foo" },
+//     { date: '2016-12-03', someAttr: "bar" },
+//     { date: '2016-12-03', someAttr: "baz" },
+//     // ...and so on
+//   ]}
+// />
