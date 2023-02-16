2745636e3cc644cfe7d17efa495aae91a5612bb0
Abhishek <abhi.jaiswal1494@gmail.com>
2023-02-16 21:18:28 +0530

    16/2

--- git_calender/git_calender.js
+++ git_calender/git_calender.js
@@ -3,3 +3,9 @@ GitHubCalendar(".calendar", "abhishek1494k");
 // or enable responsive functionality:
 GitHubCalendar(".calendar", "abhishek1494k", { responsive: true });
 
+// Use a proxy
+// GitHubCalendar(".calendar", "abhishek1494k", {
+//   proxy(abhishek1494k) {
+//     return fetch(`https://your-proxy.com/github?user=${abhishek1494k}`);
+//   },
+// }).then((r) => r.text());
