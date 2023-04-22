a278ee02446dd0d3e958b0e4f7e8b07180ae0c05
Abhishek <abhi.jaiswal1494@gmail.com>
2023-04-22 16:20:43 +0530

    Navbar codes changed due to responsiveness errors

---
+++ script/navBar.js
@@ -0,0 +1,20 @@
+function resume() {
+    window.open("https://drive.google.com/file/d/1DPUbSCRDJbNd2Su1NAzYhed_QRKdp1gG/view?usp=sharing", "_blank");
+}
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
